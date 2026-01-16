54f75895cedcd919b782e688f293d3c024799e4e
Nikkayowill <w0439161@nscc.ca>
2026-01-15 22:36:04 -0400

    Initial MVP: Express server with nav, pages, contact form

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/about', (req, res) => res.sendFile(__dirname + '/about.html'));
+app.get('/contact', (req, res) => res.sendFile(__dirname + '/contact.html'));
+
+app.use(express.urlencoded({ extended: false })); // lets us read form bodies
+app.post('/contact', (req, res) => {
+  console.log('Form received:', req.body);
+res.redirect('/');
+});
+
+app.get('/terms', (req, res) => res.sendFile(__dirname + '/terms.html'));
+app.listen(3000, () => console.log('Server on http://localhost:3000'));
