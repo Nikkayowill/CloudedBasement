6b122672edba217d2276ad6a33cac2fb75d0e743
Nikkayowill <w0439161@nscc.ca>
2026-01-19 01:45:28 -0400

    fix: add tailwind.config.js with proper content paths to scan all template files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./controllers/**/*.js",
+    "./helpers.js",
+    "./index.js",
+    "./routes/**/*.js",
+    "./public/**/*.html",
+    "./*.html"
+  ]
+}
